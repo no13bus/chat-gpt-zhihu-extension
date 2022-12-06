879650432ba5e4675a8a88376ea4d75bb852db80
no13bus <no13bus@no13buss-MacBook-Pro.local>
2022-12-06 22:56:37 +0800

    chrome plugin for zhihu

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# chat-gpt-zhihu-extension
-ChatGPT的知乎回答小帮手，利用AI来协助你回答知乎问题
+# 插件说明
+
+一个知乎的 chatgpt 浏览器小插件，可作为你回复问题的参考，也可以复制 chatgpt 的结果到回答的编辑器中进行相应的修改。
+
+注意: 插件使用的前提是你需要有一个自己的 chatgpt 账号, 注册地址 https://chat.openai.com/)
+
+## 软件截图和操作视频
+
+[操作视频](https://www.bilibili.com/video/BV1T84y1r7wy/?share_source=copy_web&vd_source=ae0fecf5ed4742e42e47340480aa174f)
+
+![Screenshot](screenshot.png?raw=true)
+
+## 安装
+
+### chrome 应用商店安装(todo 审核中)
+
+### 本地安装
+
+1. 下载 `chrome.zip` from [Releases](https://github.com/no13bus/chat-gpt-zhihu-extension/releases)
+2. 解压 zip
+3. 打开网址 `chrome://extensions`.
+4. 打开开发者模式.
+5. 将解压包拖到当前页面，或者手动引入也可以.
+
+## 本地构建
+
+1. clone
+2. 运行命令 `npm install`
+3. 执行 `./build.sh`
+4. 加载 `build` 文件夹到你的 chrome 中
+
+## todos
+
+- [ ] chrome 应用市场审核通过
+- [ ] 直接在回答的编辑器里面增加按钮，点击按钮即可把 chatgpt 的回答放置到编辑器中。
+
+## 致谢
+
+项目的想法来自于 wong2 的项目 [wong2/chat-gpt-google-extension](https://github.com/wong2/chat-gpt-google-extension), 感谢他做了这么棒的项目，让我看到了更多的可能性。
+
+我也在想，未来会不会我们自己也分辨不清，有些文章和回答是人写的呢，还是机器写的呢？whatever.
---
+++ src/content-script/index.mjs
@@ -0,0 +1,63 @@
+import Browser from "webextension-polyfill";
+
+async function run(question) {
+  const container = document.createElement("div");
+  container.className = "chat-gpt-zhihu";
+  container.innerHTML =
+    "<div class='chat-gpt-zhihu-header'>Waiting for ChatGPT response...</div>";
+
+  const firstCard = document.querySelector(".Question-sideColumn .Card");
+  firstCard.parentNode.insertBefore(container, firstCard);
+
+  const port = Browser.runtime.connect();
+  port.onMessage.addListener(function (msg) {
+    if (msg.answer) {
+      console.log(1, msg.answer);
+      const svgIcon = `
+      <div class="chat-gpt-zhihu-tooltip">
+        <svg class="chat-gpt-zhihu-header-icon copy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+        </svg>
+        <svg class="chat-gpt-zhihu-header-icon ok" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+        </svg>
+        <span class="tooltiptext">Copy</span>
+      </div>
+    `;
+      container.innerHTML = `
+          <div class="chat-gpt-zhihu-header">
+            <div class="chat-gpt-zhihu-header-text ">ChatGPT: </div>
+            ${svgIcon}
+          </div>
+          <pre class="chat-gpt-zhihu-answer"></pre>
+          `;
+      container.querySelector(".chat-gpt-zhihu-answer").textContent =
+        msg.answer;
+      container
+        .querySelector(".chat-gpt-zhihu-header-icon.copy")
+        .addEventListener("click", async (e) => {
+          await navigator.clipboard.writeText(msg.answer);
+          e.target.style.display = "none";
+          container.querySelector(
+            ".chat-gpt-zhihu-header-icon.ok"
+          ).style.display = "inline-block";
+        });
+    } else if (msg.error === "UNAUTHORIZED") {
+      container.innerHTML = `<div class='chat-gpt-zhihu-header'>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</div>`;
+    } else {
+      container.innerHTML =
+        "<div class='chat-gpt-zhihu-header'>Failed to load response from ChatGPT</div>";
+    }
+  });
+  port.postMessage({ question });
+}
+
+// zhihu title
+const title =
+  document.querySelector(".QuestionHeader-title")?.textContent || "";
+// zhihu sub-title
+const subTitle = document.querySelector(".QuestionRichText")?.textContent || "";
+
+window.onload = function () {
+  run(`${title} ${subTitle}`);
+};
